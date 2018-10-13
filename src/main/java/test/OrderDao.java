d1cf7a5af824b5a1a62bf8602857aabb1a8de5a8
Mike <tallinn@gmail.com>
2018-10-13 19:06:56 +0300

    hw4 refactored

--- src/main/java/test/OrderDao.java
+++ src/main/java/test/OrderDao.java
@@ -1,4 +1,4 @@
-package servlet;
+package test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
--- src/main/java/test/Servlet.java
+++ src/main/java/test/Servlet.java
@@ -1,4 +1,4 @@
-package servlet;
+package test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
